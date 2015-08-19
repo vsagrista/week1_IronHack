05a28bb7e6433e4bea79180e0e93a956bc75b347
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-19 11:05:35 +0200

    Added count words methods, cleaned unnecesary code

--- command_line_tool.rb
+++ command_line_tool.rb
@@ -22,13 +22,29 @@ class CommandLine
 	    else
 	    	puts "Sorry, you introduced an incorrect user name or password \n"
 	    	get_user_info
-	    	enters_text
+	    	choose_what?
 	    end
 	end
-
 	def count_words
 		puts @user_text.split.size
 	end
+
+	def count_letters
+		puts @user_text.split.join.length	
+	end
+
+	def reverse_letters
+		puts @user_text.reverse
+	end
+
+	def to_upcase
+		puts @user_text.upcase 
+	end
+
+	def lower_case
+		puts @user_text.downcase
+	end
+
 end
 
 
@@ -39,5 +55,5 @@ end
 command_line = CommandLine.new
 command_line.get_user_info
 command_line.enters_text
-command_line.count_words
+command_line.choose_what?
 
