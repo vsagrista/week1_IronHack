61e32a5737fccb95011cc5369e3e7cff9ddf37c7
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-19 12:09:40 +0200

    Added all methods to the program

--- command_line_tool.rb
+++ command_line_tool.rb
@@ -1,5 +1,6 @@
 
 class CommandLine
+	
 	attr_accessor :input_user_name, :input_password
 	attr_accessor :user_text
 	
@@ -16,17 +17,21 @@ class CommandLine
 	end
 
 	def enters_text
+
 		if @input_user_name == @user_name && @input_password == @password
-			puts "Please write something"
+			puts "Please write something\n"
 			@user_text = gets.chomp
 	    else
 	    	puts "Sorry, you introduced an incorrect user name or password \n"
 	    	get_user_info
+	    	enters_text
 	    	choose_what?
 	    end
+
 	end
+
 	def count_words
-		puts @user_text.split.size
+		puts @user_text.split.size   # this one doesn't end the program
 	end
 
 	def count_letters
@@ -45,12 +50,30 @@ class CommandLine
 		puts @user_text.downcase
 	end
 
+	def choose_what?
+
+		puts "\nPlease, choose what to do with your text"
+		puts "Choose between: count words, count letters, reverse letters, upcase letters or downcase latters"
+		
+		@which_method = gets.chomp.upcase
+
+		if @which_method == "count words".upcase  
+			count_words
+		elsif @which_method == "count letters".upcase  
+			count_letters
+		elsif @which_method == "reverse letters".upcase 
+			reverse_letters
+		elsif @which_method == "upcase letters".upcase 
+			to_upcase
+		elsif @which_method == "downcase letters".upcase 
+			lower_case
+		else puts "\nYou forgot to type an option."
+			choose_what?	
+		end
+
+	end
+
 end
-
-
-
-
-
 
 command_line = CommandLine.new
 command_line.get_user_info
