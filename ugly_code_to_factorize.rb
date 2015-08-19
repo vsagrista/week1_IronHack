3ed99b375a6f5b09eb2861c1a785797d7f18786a
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-19 17:51:39 +0200

    Factorized code from file

--- ugly_code_to_factorize.rb
+++ ugly_code_to_factorize.rb
@@ -1,30 +1,52 @@
-if output == :screen
-  puts text
-elsif output == :logfile
-  IO.write("log.log", text)
-elsif output == :message_queue
-  Queue.put(text)
-elsif output == :json_endpoint
-  HTTP.post(text)
-elsif output == :logstash
-  Logstash.add(text)
-elsif output == :logwatcher
-  LogWatch.new.post(text)
-elsif output == :logfile
-  IO.write("log.log", text)
-elsif output == :message_queue
-  Queue.put(text)
-elsif output == :json_endpoint
-  HTTP.post(text)
-elsif output == :logstash
-  Logstash.add(text)
-elsif output == :logwatcher
-  LogWatch.new.post(text)
-elsif output == :logfile
-  IO.write("log.log", text)
-elsif output == :message_queue
-  Queue.put(text)
-elsif output == :json_endpoint
-  HTTP.post(text)
+
+class Factorized
+
+  def initialize (output)
+    @output = output  
+    what_to_do  
+  end
+
+  def write_file
+    IO.write("log.log", text)
+  end
+
+  def add_queue
+    Queue.put(text)
+  end
+
+  def post_http
+    HTTP.post(text)          
+  end
+
+  def add_logstach
+      Logstash.add(text)
+  end
+
+  def new_logWatch
+     LogWatch.new.post(text)
+  end
+
+  def what_to_do
+    
+    case @output
+
+      when :logfile
+        write_file
+      when :message_queue
+        add_queue
+      when :json_endpoint
+        post_http
+      when :logstash
+        add_logstach
+      when :LogWatch
+        new_logWatch
+    
+    end
+  
+  end
+
 end
-Add Comment
+
+
+
+
