4112e5cddb28e47168ac5441f56ed12e88b9a64c
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-20 23:28:20 +0200

    Created classes to get input and check whether user wants auto mode or manual

--- keynote.rb
+++ keynote.rb
@@ -6,4 +6,27 @@ class ParseSlides
 		@auto = false
 	end
 
+	def get_input
+		puts "Enter n / p. Typing auto will make it work automatically"
+		direction = gets.chomp
+		  case direction
+		    when "n"
+		  		@page += 2
+		  	when "p"
+		  		@page -= 2
+		  	when "auto"
+		  		auto_mode
+		  end
+		  if_undefined_page
+	end
+
+	def manual_or_auto
+		if @auto == true 
+			sleep(3)  
+			auto_mode
+		else 
+			get_input
+		end
+	end
+
 end
