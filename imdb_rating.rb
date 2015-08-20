84a1b1c1aee06958fd5ec022e6a154a2a523b48e
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-20 22:41:32 +0200

    Changed Class to GetMovies and improved performance, deleted array

--- imdb_rating.rb
+++ imdb_rating.rb
@@ -1,48 +1,15 @@
 
 require "Imdb"
 
-
-class Movie
-	def rating(movie)
-		movie = Imdb::Search.new(movie)
-		@rating =  movie.movies[0].rating.round
-		
-	end
-	def stars
-		star = ("*" * @rating).split("")
-		puts star
-	end
-end
-
-
-star_trek = Movie.new
-star_trek.rating("Star Trek")
-star_trek.stars
-
-
-
-class Print
+class GetMovies
+attr_accessor :movies
 	def initialize
-		@max_rating = 8
-		@array = [7,8,8,3,2,6,1]
+		@movies = IO.readlines("movies.txt")
 	end
-	def print_value
-		while @max_rating > 0
-			print "|"	
-			@array.each do |item|
-			  if item >= @max_rating
-			    print "#"
-			    print "|"
-			  else 
-			  	print " "
-			  	print "|"
-			  end  
-		  end	
-		  print "\n"  
-		  @max_rating -= 1
+	def send_movies_DB
+		@movies.each do |movie|
+			GetRatings.new.rating_push(movie)
 		end
-	end	
+	end
 end
 
-new_print = Print.new
-new_print.print_value
