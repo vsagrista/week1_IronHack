0896661a86d56355e32ba9a90d25b3bd3a6e3c9f
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-20 23:34:44 +0200

    Added methods to print required slides

--- keynote.rb
+++ keynote.rb
@@ -1,5 +1,4 @@
-class ParseSlides
-	
+class ParseSlides	
 	def initialize
 		@slides = IO.readlines("slides.txt")
 		@page = 0
@@ -29,8 +28,6 @@ class ParseSlides
 		end
 	end
 
-
-
 	def fixes_undefined_input 
 		if @page == 6
 		  @page = 0
@@ -40,4 +37,44 @@ class ParseSlides
 		count_spaces
 	end	
 
+	def auto_mode
+		@auto = true
+			while true do
+			  next_slide_auto_mode
+			end
+	end
+
+	def next_slide_auto_mode
+		@page += 2
+		fixes_undefined_input
+	end
+
+	def count_spaces
+		@lines = TermInfo.screen_size[0]
+		@spaces = TermInfo.screen_size[1]
+		@times_break = (@lines -2)/2
+		@times_space = (@spaces - @slides[0].length) / 2 
+		print_page
+	end
+
+	def print_page
+		puts "\n" * @times_break
+		print " " * @times_space
+		puts @slides[@page]	
+		print " " * @times_space
+		print_type_of_mode
+		puts "\n" * (@times_break - 1)
+		puts "< PREVIOUS"
+		print " " * (@spaces - "NEXT >".length)
+		puts "NEXT >"
+		manual_or_auto
+	end
+
+	def print_type_of_mode
+		if @auto == true
+			puts "-Auto mode on-"
+		else
+			puts ">"
+		end
+	end
 end
