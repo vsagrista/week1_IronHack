6c52ccc23727306c1ff2311fad27442c5ee8a887
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-21 16:05:34 +0200

    Created class to move positions diagonally

--- chess_validator.rb
+++ chess_validator.rb
@@ -25,6 +25,58 @@ class MoveLeftRight < MoveUpDown
 	end   
 end
 
+
+class MoveDiagonal
+	def initialize
+		@direction = ""
+	end
+
+	def which_direction(array1,array2)
+		if array2[0] < array1[0] && array2[1] > array1[1]
+			@direction = "NW"
+		elsif array2[0] > array1[0] && array2[1] > array1[1]
+			@direction = "NE"
+		elsif array2[0] > array1[0] && array2[1] < array1[1]
+			@direction = "SE"
+		elsif array2[0] < array1[0] && array2[1] > array1[1]
+			@direction = "SW"
+		end
+		coordenates_calculator(array1,array2)
+	end
+	
+	def coordenates_calculator(array1,array2)
+		8.times do 
+			valid_diagonal?(array1[0],array2[0],array1[1],array2[1])
+			case @direction
+			  when "NW" 
+				array1[0]-=1
+			    array1[1]+=1
+			  when "NE"
+			  	array1[0]+=1
+			    array1[1]+=1
+			  when "SE"
+			  	array1[0]+=1
+			    array1[1]-=1
+			  when "SW"
+			  	array1[0]-=1
+			    array1[1]-=1
+			end
+		end
+	end
+	def valid_diagonal?(x,y,z,n)
+		if x == y && z == n
+			puts "Valid"
+			return true
+		end
+	end
+end
+
+
 new_move = MoveLeftRight.new
 p new_move.valid_y([3,4],[5,3])
 p new_move.valid_x([0,4],[3,4])
+
+
+move = MoveDiagonal.new 
+p move.which_direction([3,2],[4,1])
+
