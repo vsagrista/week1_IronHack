d91b3c0a466036ede5a02bce2341420b3d363dda
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-21 16:03:48 +0200

    Created class to move positions sideways and inherites up and down moves class

--- chess_validator.rb
+++ chess_validator.rb
@@ -11,3 +11,20 @@ class MoveUpDown  #[2,3],[2,4]
 		end
 	end   
 end
+
+
+class MoveLeftRight < MoveUpDown
+	def valid_x(array1,array2) # compare x with change in y
+		if array1[1] == array2[1] 
+			if array1[0] != array2[0]
+				true
+			else 
+				false
+			end	
+		end
+	end   
+end
+
+new_move = MoveLeftRight.new
+p new_move.valid_y([3,4],[5,3])
+p new_move.valid_x([0,4],[3,4])
